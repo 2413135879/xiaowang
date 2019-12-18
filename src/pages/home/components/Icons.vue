447c08e47954d89ad97f49d374b742c16bf489c8
2413135879 <2413135879@qq.com>
2019-12-18 15:46:46 +0800

    hot done

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,8 +1,9 @@
 <template>
+<!--icons最外层-->
  <div class="icons">
         <swiper :options="swiperOption">
             <swiper-slide v-for='(page, index) of pages' :key='index'>
-
+   
                 <div class="icon" v-for="item of page" :key="item.id">
              
                     <div class="icon-img">
@@ -68,7 +69,7 @@ export default {
     pages () {
       const pages = []
       this.iconList.forEach((item, index) => {
-       
+       //index是图标数组的索引，利用索引除以8来计算是否分页
         const page = Math.floor(index / 8)
         if (!pages[page]) {
           pages[page] = []
@@ -82,36 +83,36 @@ export default {
 </script>
 
 <style lang='stylus' scoped>
-  .icons
-    overflow: hidden
-    height: 0
-    padding-bottom: 50%
-    background: #eee
-   
-    .icon
-      position: relative
-      overflow: hidden
-      float: left
-      width: 25%
-      height: 0
-      padding-bottom: 25%
-      .icon-img
-        position: absolute
-        top: 0
-        left: 0
-        right: 0
-        bottom: .44rem
-        box-sizing: border-box
-        .icon-img-content
-          display: block
-          margin: 0 auto
-          height: 100%
-      .icons-desc
-        position: absolute
-        left: 0
-        right: 0
-        bottom: 0
-        height: .44rem
-        line-height: .44rem
-        text-align: center
+  //@import '~styles/varibles.styl';
+     .icons >>> .swiper-container
+       height 0
+       padding-bottom 50%
+       background #eee
+     .icon
+       overflow hidden
+       float left
+       width 25%
+       height 0
+       padding-bottom 25%
+       position relative
+       .icon-img
+         position absolute
+         top 0
+         left 0
+         right 0
+         bottom .44rem
+         box-sizing border-box
+         .icon-img-content
+           display block
+           margin 0 auto
+           height 100%
+        .icon-desc
+          position absolute
+          left 0
+          right 0
+          bottom 0
+          height .44rem
+          line-height .44rem
+          text-align center
+          ellipsis()
 </style>
